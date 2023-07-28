c2a073910bc82975e6c86c9d446e2316c16c4716
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2023-07-28 14:33:36 -0600

    baseline complete

--- main.js
+++ main.js
@@ -15,28 +15,28 @@ async function connect() {
 
     while (true) {
         const value = await command('RT');
-        if (value.length == 6 && !value.includes('\r') && !value.includes('\n')) {
-            data.push(parseFloat(value))
+        if (value.length == 6 && !value.includes('\r') && !value.includes('\n') && value != '100.00') {
+            data.push(parseFloat(value));
         }
     }
 }
 
 async function command(option) {
     if (option == 'RT') {
-        return await response('T1')
+        return await response('T1');
     } else if (option == 'HON') {
-        await response('Q1 100')
+        await response('Q1 100');
     } else if (option == 'HOF') {
-        await response('Q1 0')
+        await response('Q1 0');
     } else if (option == 'LON') {
-        await response('LED 100')
+        await response('LED 100');
     } else if (option == 'LOF') {
-        await response('LED 0')
+        await response('LED 0');
     }
 }
 
-async function response(command) {
-    await writer.write(command+'\n')
+async function response(key) {
+    await writer.write(key+'\n');
     await new Promise(r => setTimeout(r, 50));
 
     let current = await reader.read();
