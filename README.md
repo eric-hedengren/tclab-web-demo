6c0a3a2076efb1f2195e5831542a862dad62c960
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2023-07-28 11:38:15 -0600

    web demo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### TCLab PyScript
 This repository is an attempt to rewrite the [TCLab](https://github.com/jckantor/TCLab) python package into a functional package for the web. This will increase user friendliness when new learners are starting to code for the first time, using their [Temperature Control Labs](http://apmonitor.com/pdc/index.php/Main/ArduinoTemperatureControl).
+
+#### Web Demo
+Before fully integrating the TCLab package into the web, a simple web demo has been created to show its future potential, and also give new users an easy way to show that their TCLab actually works.
---
+++ backend.js
@@ -0,0 +1,32 @@
+let writer, reader, value;
+
+async function connect() {
+    const port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 115200 });
+
+    const textEncoder = new TextEncoderStream();
+    textEncoder.readable.pipeTo(port.writable);
+    writer = textEncoder.writable.getWriter();
+
+    const textDecoder = new TextDecoderStream();
+    port.readable.pipeTo(textDecoder.writable);
+    reader = textDecoder.readable.getReader();
+}
+
+async function read(command) {
+    await writer.write(command);
+    await new Promise(r => setTimeout(r, 50));
+    let data = await reader.read();
+
+    while (data.value.indexOf('\r\n') != 6) {
+        await new Promise(r => setTimeout(r, 50));
+        let extra = await reader.read();
+        data.value += extra.value;
+    }
+    await new Promise(r => setTimeout(r, 50));
+    value = data.value.replace('\r\n', '')
+}
+
+function give() {
+    return value
+}
