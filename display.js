78a985e8dd3d03454d3ddcbc8be01ab37de21776
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2023-08-17 13:47:31 -0600

    styles

--- display.js
+++ display.js
@@ -1,14 +1,17 @@
 const line = new TimeSeries();
+const primary = getComputedStyle(document.querySelector(':root')).getPropertyValue('--primary')
+const highlight = getComputedStyle(document.querySelector(':root')).getPropertyValue('--highlight')
+const standard = getComputedStyle(document.querySelector(':root')).getPropertyValue('--standard')
 
 setInterval(async function() {
     const value = await command('R');
-    if (value && value.length == 6 && !value.includes('\r') && !value.includes('\n') && value != '100.00') {
+    if (value && value.length == 6 && !value.includes('\r') && !value.includes('\n') && value != '100.00' && value != '00.000') {
         line.append(Date.now(), value);
     }
 }, 1000);
 
 function createChart() {
-    const chart = new SmoothieChart({ responsive: true });
-    chart.addTimeSeries(line);
+    const chart = new SmoothieChart({responsive: true, grid: {strokeStyle: standard}, labels: {fillStyle: highlight}});
+    chart.addTimeSeries(line, {lineWidth: 4, strokeStyle: primary, fillStyle: primary.substring(0,primary.length-1)+', .4)'});
     chart.streamTo(document.getElementById("chart"), 1000);
 }
