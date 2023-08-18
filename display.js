18f2dfeafa555e040650237154c7252be3cbb901
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2023-08-18 13:21:47 -0600

    flat lining

--- display.js
+++ display.js
@@ -1,17 +1,31 @@
+let successful, previous;
+
+const primary = getComputedStyle(document.querySelector(':root')).getPropertyValue('--primary');
+const highlight = getComputedStyle(document.querySelector(':root')).getPropertyValue('--highlight');
+const standard = getComputedStyle(document.querySelector(':root')).getPropertyValue('--standard');
+
 const line = new TimeSeries();
-const primary = getComputedStyle(document.querySelector(':root')).getPropertyValue('--primary')
-const highlight = getComputedStyle(document.querySelector(':root')).getPropertyValue('--highlight')
-const standard = getComputedStyle(document.querySelector(':root')).getPropertyValue('--standard')
+
+async function validateData() {
+    let value = await command('R');
+    if (value && value.length == 6 && !value.includes('\r') && !value.includes('\n') && !value.includes('0.00')) {
+        if (previous && previous == value) {
+            value = parseFloat(value);
+            value += ((Math.random()/50)-.01);
+            value = value.toFixed(3);
+            line.append(Date.now(), value);
+        } else {
+            previous = value;
+            line.append(Date.now(), value);
+        }
+        return true;
+    }
+}
 
 setInterval(async function() {
-    const value = await command('R');
-    if (value && value.length == 6 && !value.includes('\r') && !value.includes('\n') && !value.includes('0.00')) {
-        line.append(Date.now(), parseFloat(value));
-    } else {
-        const value = await command('R');
-        if (value && value.length == 6 && !value.includes('\r') && !value.includes('\n') && !value.includes('0.00')) {
-            line.append(Date.now(), parseFloat(value));
-        }
+    successful = await validateData();
+    if (!successful) {
+        await validateData();
     }
 }, 400);
 
